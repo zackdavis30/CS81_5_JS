49ed0d9a993f5b897e6e1266289289b3172bf7f3
Zack <32910932+zackdavis30@users.noreply.github.com>
2018-08-23 16:35:16 -0700

    Add files via upload

---
+++ CS81_5_ZacharyDavis_Problem2.js
@@ -0,0 +1,34 @@
+
+/* Orignal code 
+function unless(test, then) {
+	if (!test) then();
+}
+
+function repeat(times, body) {
+	for (var i = 0; i < times; i++) body(i);
+
+}
+repeat(3, function(n) {
+unless(n % 2, function() {
+console.log(n, "is even");
+});
+}); */
+
+
+//modified code
+function unless(test, then) {
+	if (test) then();
+
+}
+function repeat(times, body) {
+	for (var i = 0; i < times; i++) body(i);
+
+}
+
+repeat(50, function(n) {
+unless(n % 3 === 2 , function() {
+console.log(n );
+
+});
+
+});
